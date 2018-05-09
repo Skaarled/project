3ad75f2415b184c76c0f72629b6f055070bf2f70
Skaarled <37436325+Skaarled@users.noreply.github.com>
2018-05-09 20:15:01 +0700

    Update view.h

--- view.h
+++ view.h
@@ -3,7 +3,7 @@
 #include "models.h"
 
 void printBackground ();
-void printDetails (int level,string typeOfWord, string word, int pointForGuess[10], bool boolPointForGuess[10], string suggetWord[10], bool boolSuggetWord[10],int pointLevel, bool boolPoint[10]);
+void printDetails (int level,string typeOfKeyWord, string word, int guessPoint[10], bool boolGuessPoint[10], string suggetWord[10], bool boolSuggetWord[10],int pointLevel, bool boolPoint[10]);
 void printStartScreen ();
 
 #endif // VIEW_H
